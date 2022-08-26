91ff15ff02963090181b3e766a068ecae762ec5b
kdoni <chrisvdoni@gmail.com>
2022-08-26 23:57:27 +0300

    tough rough ahead...

--- scripts.js
+++ scripts.js
@@ -10,7 +10,7 @@ let clickable = document.getElementsByClassName("push");
 
 
 let accumulator = 0;
-let currentValue = 0;
+let reset = 0;
 let sum = 0;
 
 //Basic calculations//
@@ -19,24 +19,19 @@ function calculations(a) {
     let splitted = a.split("");
     splitted.pop();
     let joined = splitted.join("");
-    // console.log(joined);
-
+    let additive = joined.slice(joined.lastIndexOf("+") + 1);
+    let negative = joined.slice(joined.lastIndexOf("-"));
     if (!joined.includes("+") && !joined.includes("-") && !joined.includes("x")) {
         sum = parseInt(joined);
         return sum;
-    } else if (joined.includes("+")) {
-        console.log(joined);
-        let test = joined.split("+");
-        sum = test.reduce((c, d) => parseInt(c) + parseInt(d));
-        // console.log(joined);
-        return sum;
-        // let test = joined.split(/[-+*/]/);
-    } else if (joined.includes("-")) {
-        console.log(joined);
-        let subtractee = joined.slice(joined.lastIndexOf("-") + 1);
-        console.log(subtractee);
     }
-
+    if (joined.includes("+")) {
+        sum += parseInt(additive);
+        joined = joined.split("").splice(0, 2, sum);
+        return sum;
+    } else if (joined.includes("-")) {
+        console.log("nega");
+    }
 }
 
 //Enable & disable button functions 
@@ -78,7 +73,7 @@ for (let i = 0; i < quickMath.length; i++) {
         if (currentValue[currentValue.length - 1] === "+" || currentValue[currentValue.length - 1] === "-" || currentValue[currentValue.length - 1] === "x" || currentValue[currentValue.length - 1] === "&divide;") {
             value.style.display = "block";
             value.textContent = calculations(currentValue);
-            console.log(sum);
+            // console.log(sum);
         }
     })
 }
