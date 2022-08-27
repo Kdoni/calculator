1f7d05b9f3d721b8367fa0c748795015162155b8
kdoni <chrisvdoni@gmail.com>
2022-08-27 15:11:11 +0300

    That kind of works...

--- scripts.js
+++ scripts.js
@@ -10,28 +10,51 @@ let clickable = document.getElementsByClassName("push");
 
 
 let accumulator = 0;
-let reset = 0;
 let sum = 0;
 
 //Basic calculations//
 
 function calculations(a) {
+    let numberArray = [];
+    let operatorArray = [];
     let splitted = a.split("");
     splitted.pop();
     let joined = splitted.join("");
-    let additive = joined.slice(joined.lastIndexOf("+") + 1);
-    let negative = joined.slice(joined.lastIndexOf("-"));
-    if (!joined.includes("+") && !joined.includes("-") && !joined.includes("x")) {
+    // let additive = joined.slice(joined.lastIndexOf("+") + 1);
+    // let negative = joined.slice(joined.lastIndexOf("-"));
+    if (!joined.includes("+") && !joined.includes("-") && !joined.includes("x") && !joined.includes("÷")) {
         sum = parseInt(joined);
         return sum;
     }
-    if (joined.includes("+")) {
-        sum += parseInt(additive);
-        joined = joined.split("").splice(0, 2, sum);
-        return sum;
-    } else if (joined.includes("-")) {
-        console.log("nega");
+    let numbers = joined.split(/[\+\-x\/÷]/g);
+    let operators = joined.split(/[0-9]+/).filter(word => word !== "");
+    if (operators[0] === "+") {
+        accumulator = Number(numbers[0]) + Number(numbers[1]);
+    } else if (operators[0] === "-") {
+        accumulator = Number(numbers[0]) - Number(numbers[1]);
+    } else if (operators[0] === "x") {
+        accumulator = Number(numbers[0]) * Number(numbers[1]);
+    } else if (operators[0] === "÷") {
+        accumulator = (Number(numbers[0]) / Number(numbers[1])).toFixed(2);
+        if (Number(numbers[1] === "0")) {
+            return "cringe...";
+        }
     }
+    for (let i = 2; i < numbers.length; i++) {
+        if (operators[i - 1] === "+") {
+            accumulator = accumulator + (Number(numbers[i]));
+        } else if (operators[i - 1] === "-") {
+            accumulator = accumulator - (Number(numbers[i]));
+        } else if (operators[i - 1] === "x") {
+            accumulator = accumulator * Number(numbers[i]);
+        } else if (operators[i - 1] === "÷") {
+            accumulator = (accumulator / Number(numbers[i])).toFixed(2);
+            if (Number(numbers[i] === "0")) {
+                return "cringe...";
+            }
+        }
+    };
+    return accumulator;
 }
 
 //Enable & disable button functions 
@@ -70,10 +93,10 @@ for (let i = 0; i < clickable.length; i++) {
 for (let i = 0; i < quickMath.length; i++) {
     quickMath[i].addEventListener("click", function (e) {
         currentValue = display.textContent;
-        if (currentValue[currentValue.length - 1] === "+" || currentValue[currentValue.length - 1] === "-" || currentValue[currentValue.length - 1] === "x" || currentValue[currentValue.length - 1] === "&divide;") {
+        if (currentValue[currentValue.length - 1] === "+" || currentValue[currentValue.length - 1] === "-" || currentValue[currentValue.length - 1] === "x" || currentValue[currentValue.length - 1] === "÷") {
             value.style.display = "block";
             value.textContent = calculations(currentValue);
             // console.log(sum);
         }
     })
-}
+};
